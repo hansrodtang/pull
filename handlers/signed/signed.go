f0690b8e097d5a3ab16f94d1ec41573bf1222780
Hans Rødtang <hansrodtang@gmail.com>
2015-06-30 20:03:44 +0200

    Fixed spelling and added context to API request.

--- handlers/signed/signed.go
+++ handlers/signed/signed.go
@@ -18,15 +18,14 @@ func Signed(client *github.Client, event *github.PullRequest) {
 		match, _ := regexp.Match(r, []byte(*c.Commit.Message))
 
 		if match {
-			success := pull.Success("", "Commit is signed-off")
+			success := pull.Success("", "Commit is signed off", "signed")
 			client.Repositories.CreateStatus(user, repo, *c.SHA, success)
 		} else {
 			s := fmt.Sprintf("Commit not signed off: [%s](%s).", *c.SHA, *c.HTMLURL)
 			comment := &github.IssueComment{Body: &s}
 
 			cm, _, _ := client.Issues.CreateComment(user, repo, number, comment)
-			failure := pull.Failure(*cm.HTMLURL, "Commit is not signed-off")
-
+			failure := pull.Failure(*cm.HTMLURL, "Commit is not signed off", "signed")
 			client.Repositories.CreateStatus(user, repo, *c.SHA, failure)
 		}
 	}
