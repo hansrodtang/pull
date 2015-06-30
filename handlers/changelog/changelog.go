6ec8a9279c6ad6596cd86a36416daeabbce1e95d
Hans Rødtang <hansrodtang@gmail.com>
2015-06-30 20:00:44 +0200

    Add ChangeLog enforcer middleware.

---
+++ handlers/changelog/changelog.go
@@ -0,0 +1,35 @@
+package changelog
+
+import (
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/hansrodtang/pull"
+)
+
+func New(match string) pull.Middleware {
+	return func(client *github.Client, event *github.PullRequest) {
+		user, repo, number := pull.Data(event)
+
+		commits, _, _ := client.PullRequests.ListCommits(user, repo, number, nil)
+		files, _, _ := client.PullRequests.ListFiles(user, repo, number, nil)
+
+		found := false
+		for _, file := range files {
+			found = strings.EqualFold(*file.Filename, match)
+		}
+
+		var message *github.RepoStatus
+		if found {
+			message = pull.Success("", "ChangeLog Kept", "changelog")
+		} else {
+			message = pull.Failure("", "Keep a ChangeLog", "changelog")
+		}
+
+		for _, c := range commits {
+			client.Repositories.CreateStatus(user, repo, *c.SHA, message)
+		}
+
+	}
+
+}
